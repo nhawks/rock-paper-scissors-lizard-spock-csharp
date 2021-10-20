0b584b0f500e8222e9156df36e2b0ea1d3d56dc4
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-20 19:25:57 -0400

    Update GameRules.cs

--- RPSLS/GameRules.cs
+++ RPSLS/GameRules.cs
@@ -8,5 +8,28 @@ namespace RPSLS
 {
     class GameRules
     {
+        public string gameRules = $@"
+            ____________________________________________________________________________
+            | 🥊📃✂🦎🖖 | ROCK vs PAPER vs SCISSORS vs LIZARD vs SPOCK | 🥊📃✂🦎🖖     |
+            |  The rules are simple pick an item from the list at start of each round  |
+            | Depending on your choice and your opponent's choice will decide who wins!|
+            |                   You can play against AI or a friend                    |
+            |                  Pick how many rounds you'd like to play.                |               
+            |__________________________________________________________________________|
+            |                            GESTURE BREAKDOWN                             |
+            |__________________________________________________________________________|
+            |__________________________________________________________________________|
+            |        Rock crushes Scissors         |       Scissors cuts Paper         |   
+            |--------------------------------------|-----------------------------------|    
+            |        Paper covers Rock             |       Rock crushes Lizard         | 
+            |--------------------------------------|-----------------------------------| 
+            |        Lizard poisons Spock          |    Spock smashes Scissors         |    
+            |--------------------------------------|-----------------------------------| 
+            |        Scissors decapitates Lizard   |    Lizard eats Paper              | 
+            |--------------------------------------|-----------------------------------|      
+            |        Paper disproves Spock         |    Spock vaporizes Rock           |   
+            |--------------------------------------|-----------------------------------|    
+            |__________________________________________________________________________|
+            |__________________________________________________________________________|";
     }
 }
