703b5034c5b785000149dbcddebb38ec20fcb91e
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-20 15:37:47 -0400

    Created base Player class.
    - Created member variables.
    - Created constructor method to take a player name.
    - TODO: Create methods for class.

---
+++ RPSLS/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Player
+    {
+        // Member Variables (Has)
+        public string name;
+        public string choice;
+        public int score;
+        public List<string> gestures = new List<string> { "rock", "paper", "scissors", "lizard", "spock" };
+
+        // Constructor (Generates)
+        public Player(string playerName)
+        {
+            name = playerName;
+           
+        }
+
+        // Methods (Can do)
+
+        public void pickGesture(string gesture)
+        {
+            Console.WriteLine(gestures.Contains(gesture));
+        }
+
+    }
+}
